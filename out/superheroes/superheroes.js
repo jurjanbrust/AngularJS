e65817abc53b2851ada8394befb1decbf5e15a77
jurjanbrust <jurjanbrust@hotmail.com>
2020-04-02 08:41:56 +0200

    updates to ts

---
+++ out/superheroes/superheroes.js
@@ -0,0 +1,37 @@
+var module = angular.module('superHeroesApp', []);
+const SUPERHEROS = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+class SuperHerosComponentController {
+    constructor() { }
+    $onInit() {
+        this.heros = SUPERHEROS;
+    }
+}
+class SuperHerosComponent {
+    constructor() {
+        this.controller = SuperHerosComponentController;
+        this.controllerAs = "$ctrl";
+        this.template = `
+      <ul>
+        <li ng-repeat="hero in $ctrl.heros">{{ hero.name }}</li>
+      </ul>
+    `;
+    }
+}
+angular
+    .module("mySuperAwesomeApp", [])
+    .component("superheros", new SuperHerosComponent());
+angular.element(document).ready(function () {
+    angular.bootstrap(document, ["mySuperAwesomeApp"]);
+});
+//# sourceMappingURL=superheroes.js.map
--- src/HelloWorld.ts
+++
@@ -1,2 +0,0 @@
-let message: string = 'Hello World2';
-console.log(message);
